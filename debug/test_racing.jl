74e5c90e61ed655314ee887773cbaac2bd10042e
cinaral <andcinar@gmail.com>
2024-01-23 23:29:53 -0600

    Update solve_seq_adaptive()

--- debug/test_racing.jl
+++ debug/test_racing.jl
@@ -10,12 +10,12 @@ probs = setup(; T=10,
 	β = 1e1,
 	cd = 0.01,
 	u_max_nominal = 1.0, 
-	u_max_drafting = 3.0,
+	u_max_drafting = 5.0,
 	box_length = 5.0,
 	box_width = 1.0,
 	lat_max = 1.0);
 
-#x0 = [1., 3, 0, 1, -1, 2, 0, 1]
+x0 = [1., 3, 0, 1, -1, 2, 0, 1]
 
 #sp_a fail?
 #x0 = [0.5153546193408008
