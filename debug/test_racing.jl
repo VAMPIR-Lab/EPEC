b919f340dffef4cacd159bc3d35ce1d65796d166
cinaral <andcinar@gmail.com>
2024-01-24 09:37:04 -0600

    Fix sp i racing.jl for T=/=10

--- debug/test_racing.jl
+++ debug/test_racing.jl
@@ -10,20 +10,20 @@ probs = setup(; T=10,
 	α2 = 1e-4,
 	β = 1e0,
 	cd = 0.2,
-	u_max_nominal = 2.0, 
-	u_max_drafting = 3.0,
+	u_max_nominal = 1.0, 
+	u_max_drafting = 2.0,
 	box_length = 5.0,
-	box_width = 1.0,
+	box_width = 2.0,
 	lat_max = 1.5);
 
 #x0 = [1., 3, 0, 1, -1, 2, 0, 1]
-x0 = [0, 0, 0, 1.5, 0.25, 1, 0, 1]
+x0 = [.5, 0, 0, 2, 0, 1, 0, 1]
 #(; P1, P2, gd_both, h, U1, U2, lowest_preference, sorted_Z) = solve_seq_adaptive(probs, x0);
 #(f, ax, XA, XB, lat) = visualize(; rad = sqrt(probs.params.r) / 2, lat = probs.params.lat_max + sqrt(probs.params.r) / 2);
 #display(f)
 #update_visual!(ax, XA, XB, x0, P1, P2; T = probs.params.T, lat = lat)
 
-sim_results = solve_simulation(probs, 200; x0, only_want_gnep=false);
+sim_results = solve_simulation(probs, 100; x0, only_want_gnep=true);
 animate(probs, sim_results; save=false);
 
 prefs = zeros(Int, length(sim_results))
