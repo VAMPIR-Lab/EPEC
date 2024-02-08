6bba4d2a1d0fa0c2d14111504b9cce2b86f2daec
cinaral <andcinar@gmail.com>
2024-02-08 10:18:12 -0600

    Add car shapes

--- racing/racing.jl
+++ racing/racing.jl
@@ -133,7 +133,7 @@ function g1(Z,
     u_max_drafting=5.0,
     box_length=3.0,
     box_width=1.0,
-    col_buffer=r/5)
+    col_buffer=r / 5)
     T = Int((length(Z) - 8) / 12) # 2*(state_dim + control_dim) = 12
     @inbounds Xa = @view(Z[1:4*T])
     @inbounds Ua = @view(Z[4*T+1:6*T])
@@ -176,7 +176,7 @@ function g2(Z,
     u_max_drafting=5.0,
     box_length=3.0,
     box_width=1.0,
-    col_buffer=r/5)
+    col_buffer=r / 5)
     T = Int((length(Z) - 8) / 12) # 2*(state_dim + control_dim) = 12
     @inbounds Xa = @view(Z[1:4*T])
     @inbounds Ua = @view(Z[4*T+1:6*T])
@@ -226,7 +226,7 @@ function setup(; T=10,
     box_width=1.0,
     lat_max=5.0,
     min_long_vel=-5.0,
-    col_buffer=r/5)
+    col_buffer=r / 5)
 
     lb = [fill(0.0, 4 * T); fill(0.0, T); fill(-u_max_nominal, T); fill(-Inf, 4 * T); fill(-u_max_braking, T); fill(min_long_vel, T); fill(-lat_max, T)]
     ub = [fill(0.0, 4 * T); fill(Inf, T); fill(+u_max_nominal, T); fill(0.0, 4 * T); fill(Inf, T); fill(Inf, T); fill(+lat_max, T)]
@@ -438,7 +438,7 @@ function solve_seq_adaptive(probs, x0; only_want_gnep=false, only_want_sp=false,
         else
             sp_success = θ_sp_a_success && θ_sp_b_success # consider sp success to be bilateral succes
         end
-       
+
         if sp_success
             #@info "sp success 7"
             gnep_like = zeros(probs.gnep.top_level.n)
@@ -730,10 +730,16 @@ function animate(probs, sim_results; save=false, filename="test.mp4", sleep_dura
     end
 end
 
+#using CairoMakie
+
 function visualize(; T=10, rad=0.5, lat=6.0)
-    f = Figure(resolution=(1000, 1000), grid = false)
+    f = Figure(resolution=(1000, 1000), grid=false)
     ax = Axis(f[1, 1], aspect=DataAspect())
     #resize_to_layout!(f)
+
+    carsymbol_string = "M 7 9 C 5.897 9 5 9.897 5 11 L 5 11.4375 C 6.328 12.0205 7.324 13.1585 7.75 14.5625 C 7.917 14.8145 8 15.245 8 16 L 8 18 L 11 18 L 11 19 L 13 19 L 13 18 L 15 18 C 16.103 18 17 17.103 17 16 L 17 11 C 17 9.897 16.103 9 15 9 L 7 9 z M 34 10 C 32.897 10 32 10.897 32 12 L 32 16 C 32 17.103 32.897 18 34 18 L 35 18 L 35 19 L 37 19 L 37 18 L 38 18 L 38 19 L 40 19 L 40 18 L 41 18 C 42.103 18 43 17.103 43 16 L 43 12 C 43 10.897 42.103 10 41 10 L 34 10 z M 1 13 C 0.448 13 0 13.448 0 14 L 0 36 C 0 36.553 0.448 37 1 37 L 3 37 C 4.654 37 6 35.654 6 34 L 6 16 C 6 14.346 4.654 13 3 13 L 1 13 z M 45 13 C 44.447 13 44 13.448 44 14 L 44 18.625 C 47.242 19.242 49.80475 20.10125 49.96875 20.15625 C 49.98475 20.16125 49.985 20.1825 50 20.1875 L 50 14 C 50 13.448 49.553 13 49 13 L 45 13 z M 20 15 C 19.649 15 19.33625 15.199 19.15625 15.5 L 16.4375 20 L 8 20 L 8 30 L 16.4375 30 L 19.15625 34.5 C 19.33625 34.8 19.649 35 20 35 L 27 35 C 27.208 35 27.42475 34.9345 27.59375 34.8125 L 34.3125 30 L 38 30 C 43.05 30 49.0605 28.0215 49.3125 27.9375 C 49.7235 27.8025 50 27.432 50 27 L 50 23 C 50 22.57 49.7205 22.1995 49.3125 22.0625 C 49.0635 21.9785 43.157 20 38 20 L 34.3125 20 L 27.59375 15.1875 C 27.42475 15.0665 27.208 15 27 15 L 20 15 z M 22 22 L 27 22 C 28.657 22 30 23.343 30 25 C 30 26.657 28.657 28 27 28 L 22 28 L 22 22 z M 50 29.8125 C 49.981 29.8195 49.9565 29.83775 49.9375 29.84375 C 49.7705 29.89875 47.219 30.72875 44 31.34375 L 44 36 C 44 36.553 44.447 37 45 37 L 49 37 C 49.553 37 50 36.553 50 36 L 50 29.8125 z M 11 31 L 11 32 L 8 32 L 8 34 C 8 34.496 7.97425 34.809 7.90625 35 C 7.58025 36.61 6.469 37.9185 5 38.5625 L 5 39 C 5 40.105 5.895 41 7 41 L 15 41 C 16.105 41 17 40.105 17 39 L 17 34 C 17 32.895 16.105 32 15 32 L 13 32 L 13 31 L 11 31 z M 35 31 L 35 32 L 34 32 C 32.897 32 32 32.897 32 34 L 32 38 C 32 39.103 32.897 40 34 40 L 41 40 C 42.103 40 43 39.103 43 38 L 43 34 C 43 32.897 42.103 32 41 32 L 40 32 L 40 31 L 38 31 L 38 32 L 37 32 L 37 31 L 35 31 z"
+    carsymbol = BezierPath(carsymbol_string, fit = true, flipy = true)
+
     GLMakie.lines!(ax, [-lat, -lat], [-10.0, 1000.0], color=:black)
     GLMakie.lines!(ax, [+lat, +lat], [-10.0, 1000.0], color=:black)
 
@@ -744,6 +750,12 @@ function visualize(; T=10, rad=0.5, lat=6.0)
     circ_y = [rad * sin(t) for t in 0:0.1:(2π+0.1)]
     GLMakie.lines!(ax, @lift(circ_x .+ $(XA[0][1])), @lift(circ_y .+ $(XA[0][2])), color=:blue, linewidth=5)
     GLMakie.lines!(ax, @lift(circ_x .+ $(XB[0][1])), @lift(circ_y .+ $(XB[0][2])), color=:red, linewidth=5)
+
+    a_rot = map(atan, @lift(($(XA[0][2]) - $(XA[1][2]))/($(XA[0][1]) - $(XA[1][1]))))
+    b_rot = map(atan, @lift(($(XB[0][2]) - $(XB[1][2]))/($(XB[0][1]) - $(XB[1][1]))))
+
+    GLMakie.scatter!(ax, @lift([0.0, 0.0] .+ $(XA[0][1])), @lift([0.0, 0.0] .+ $(XA[0][2])), rotations = @lift($(a_rot) .+ pi), marker = carsymbol, markersize = 80, color = :blue)
+    GLMakie.scatter!(ax, @lift([0.0, 0.0] .+ $(XB[0][1])), @lift([0.0, 0.0] .+ $(XB[0][2])), rotations = @lift($(b_rot) .+ pi), marker = carsymbol, markersize = 80, color = :red)
 
     for t in 1:T
         GLMakie.lines!(ax, @lift(circ_x .+ $(XA[t][1])), @lift(circ_y .+ $(XA[t][2])), color=:blue, linewidth=2, linestyle=:dash)
@@ -767,8 +779,8 @@ function update_visual!(ax, XA, XB, x0, P1, P2; T=10, lat=6.0)
     end
 
     GLMakie.xlims!(ax, -2 * lat, 2 * lat)
-    avg_y_pos = (x0[2] + x0[6])/2
-    GLMakie.ylims!(ax, avg_y_pos - lat , avg_y_pos + 3 * lat)
+    avg_y_pos = (x0[2] + x0[6]) / 2
+    GLMakie.ylims!(ax, avg_y_pos - lat, avg_y_pos + 3 * lat)
 end
 
 function show_me(θ, x0; T=10, t=0, lat_pos_max=1.0)
